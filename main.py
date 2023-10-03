4fc4d78058fe94bf3a96c1792d5b6c8c896716a7
Andrei Sorokin <and.sor@test.com>
2023-10-03 17:44:42 +0300

    added version 2.0

--- main.py
+++ main.py
@@ -1,67 +1,52 @@
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
 import requests
-from bs4 import BeautifulSoup as bs
 import gspread
-import fake_useragent
 
 # Заголовок с юзер-агентом
 headers = {
-    'User-agent': fake_useragent.UserAgent().random
+    'User-agent': UserAgent().random
 }
 
-# Ссылка на контракт
-url_contract = input('Введите ссылку на контракт: ')
 
-def parsing_contract(adress):
-    # Получение кода страницы
-    response = requests.get(url=adress, headers=headers).text
-    soup = bs(response, 'lxml')
+def contract_html():
+    # Запрос к странице с контрактом
+    url_contract = input('Введите ссылку на контракт: ')
+    response = requests.get(url=url_contract, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    # Сбор информации
+    contract_number = soup.find('span', 'cardMainInfo__purchaseLink distancedText').text.strip()
+    organization_name = soup.find('span', 'cardMainInfo__content').text.strip()
+    price = soup.find('span', 'cardMainInfo__content cost').text.strip()[:-2]
+    date_contract = soup.find('div', 'date mt-auto')
+    start_date, end_date = [i.find('span', 'cardMainInfo__content').text.strip()
+                            for i in date_contract.find_all('div', 'cardMainInfo__section')[:2]]
+    return [organization_name, contract_number, start_date, end_date, price, '', url_contract]
 
-    # Название организации и цена на контракт
-    organization_name = soup.find('span', class_='cardMainInfo__content').find('a').get_text().strip()
-    price = soup.find('span', class_='cardMainInfo__content cost').get_text().strip()[:-2]
 
-    # Дата заключение и исполнения контракта
-    block_data = soup.find('div', class_='date mt-auto')
-    start_data = block_data.find('span', class_='cardMainInfo__content').get_text().strip()
-    end_data = block_data.find_all('span')[3].text.strip()
-
-    # Номер контракта
-    block_contract = soup.find('div', class_='sectionMainInfo__body')
-    a = block_contract.find_all('div')[1].find('span', class_='cardMainInfo__content').text.strip().split('\n')
-    contract = a[0] + a[-1].strip()
-    # Наполнение таблицы Google
+# Наполнение таблицы Google
+def google_sheet():
     gs = gspread.service_account(filename='credits.json')  # подключаем файл с ключами и пр.
-    sheet = gs.open_by_key('Введите id таблицы')  # подключаем таблицу по ID
+    sheet = gs.open_by_key('https://docs.google.com/spreadsheets/d/*id таблицы*/')  # подключаем таблицу по ID
     worksheet = sheet.sheet1  # получаем первый лист
-    rows = [organization_name, contract, start_data, end_data, price, '', url_contract]
-    worksheet.append_row(rows, table_range='A1')
+    worksheet.append_row(contract_html(), table_range='A1')  # вносим информацию в таблицу
 
-    #форматирование ячеек
+    # форматирование ячеек
     worksheet.format(f'A{len(worksheet.get_all_values())}:G{len(worksheet.get_all_values())}', {
-        "backgroundColor": {
-            "red": 0,
-            "green": 139,
-            "blue": 139
-        },
         "horizontalAlignment": "CENTER",
         "wrapStrategy": "LEGACY_WRAP",
         "textFormat": {
-            "foregroundColor": {
-                "red": 1.0,
-                "green": 1.0,
-                "blue": 1.0
-            },
-            "fontSize": 12
+            "fontSize": 10
         }
     })
 
+
 # Вызов функции
-parsing_contract(url_contract)
+google_sheet()
 
 # Запрос на повторное использование
 while True:
     result = input('Хотите продолжить? (д / н) ').lower()
     if result != 'д':
         break
-    URL_TEMPLATE = input('Введите ссылку на контракт: ')
-    parsing_contract(url_contract)
+    google_sheet()
